d57355e206060e99754be901723d75e356c12d68
IgorGarRez <igorgarciarezende05@gmail.com>
2022-09-26 22:48:13 -0300

    Final

--- cnpjs.py
+++ cnpjs.py
@@ -17,12 +17,13 @@ def consulta_cnpj(cnpj):
     querystring = {"token": "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX", "cnpj": "06990590000123", "plugin": "RF"}
     response = requests.request("GET", url, params=querystring)
     resp = json.loads(response.text)
-    return resp['email']
+    worksheet.write(row, 0, resp['email'])
+    worksheet.write(row, 1, resp['qsa'][0]['nome'] if len(resp['qsa']) > 0 else '')
+    worksheet.write(row, 2, resp['fantasia'])
 
 
 for i in cnpj_df['cnpj']:
-    item = consulta_cnpj(i)
-    worksheet.write(row, column, item)
+    consulta_cnpj(i)
     row += 1
     time.sleep(20)
 
